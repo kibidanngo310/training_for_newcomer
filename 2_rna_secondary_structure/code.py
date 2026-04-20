4825510ba25d2bbe8c096ac5e400869d9e4378f6
murakami <nobi931dora931@icloud.com>
2026-04-20 15:16:17 +0900

    modify_4

--- 2_rna_secondary_structure/code.py
+++ 2_rna_secondary_structure/code.py
@@ -2,6 +2,18 @@ from typing import List, Tuple, Union
 import numpy.typing as npt
 import numpy as np
 
+def pair(s1: str, s2: str) -> bool:
+    result = False
+    if s1 == "A" and s2 == "U":
+        result = True
+    if s1 == "U" and s2 == "A":
+        result = True
+    if s1 == "G" and s2 == "C":
+        result = True
+    if s1 == "C" and s2 == "G":
+        result = True
+    return result
+
 def enumerate_pairs(fastafile: str) -> List[Tuple[int, int]]:
     # 課題 2-1
     seq_list = []
@@ -17,13 +29,7 @@ def enumerate_pairs(fastafile: str) -> List[Tuple[int, int]]:
     results = []
     for i in range(0, len(seq)):
         for l in range(i, len(seq)):
-            if seq[i] == "A" and seq[l] == "U":
-                results.append((i+1, l+1))
-            if seq[i] == "U" and seq[l] == "A":
-                results.append((i+1, l+1))
-            if seq[i] == "G" and seq[l] == "C":
-                results.append((i+1, l+1))
-            if seq[i] == "C" and seq[l] == "G":
+            if pair(seq[i], seq[l]):
                 results.append((i+1, l+1))
     return results
 
@@ -42,26 +48,14 @@ def enumerate_possible_pairs(fastafile: str, min_distance: int=4) -> List[Tuple[
     results = []
     for i in range(0, len(seq)):
         for l in range(i, len(seq)):
-            if seq[i] == "A" and seq[l] == "U":
-                if l - i >= min_distance:
-                    results.append((i+1, l+1))
-                
-            if seq[i] == "U" and seq[l] == "A":
-                if l - i >= min_distance:
-                    results.append((i+1, l+1))
-
-            if seq[i] == "G" and seq[l] == "C":
-                if l - i >= min_distance:
-                    results.append((i+1, l+1))
-
-            if seq[i] == "C" and seq[l] == "G":
-                if l - i >= min_distance:
+            if pair(seq[i], seq[l]):
+                if (l - i >= min_distance):
                     results.append((i+1, l+1))
     return results
 
 def enumerate_continuous_pairs(fastafile: str, min_distance: int=4, min_length: int=2) -> List[Tuple[int, int, int]]:
     # 課題 2-3
-
+    
     return []
 
 def create_dotbracket_notation(fastafile: str, min_distance: int=4, min_length: int=2) -> str:
